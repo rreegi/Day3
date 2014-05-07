b447ee6b2762b6632910f77b9af6d3b42e744745
rreegi <ram89621@gmail.com>
2014-05-07 10:10:56 -0400

    Test Project

---
+++ Day3/Day3/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Day3
+//
+//  Created by User1 on 2014-05-07.
+//  Copyright (c) 2014 Juice. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
